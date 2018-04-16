d97e3e6aee6a41f05aa293f6d8dbfe7c7634746e
Dmitry <alpha.ru@gmail.com>
2018-04-16 16:15:35 +0300

    add more worker tests

--- test/workers/get_cbr_rate_worker_test.rb
+++ test/workers/get_cbr_rate_worker_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 class GetCbrRateWorkerTest < MiniTest::Unit::TestCase
+  def after_teardown
+    Sidekiq::Worker.clear_all
+    super
+  end
+
   def test_quequeing
     GetCbrRateWorker.perform_async
     assert_equal 1, GetCbrRateWorker.jobs.size
+  end
+
+  def test_scraping
+    GetCbrRateWorker.perform_async
+    Sidekiq::Worker.drain_all
+    cbr_rate = CbrRateScraper.usd_rate
+    assert_equal cbr_rate, ExchangeRate.last.rate
   end
 end
